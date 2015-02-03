a414679b4cf54b88739f8d9f0b7c731031dab705
codeForReviewer <rragan@Roberts-Mac-Pro.local>
2015-02-03 15:47:42 -0800

    update shiny
    Now reproduces all figures in paper

--- inst/kMajorityGUI/server.R
+++ inst/kMajorityGUI/server.R
@@ -7,20 +7,81 @@ shinyServer(function(input, output) {
 
   
 simOutput <- reactive({
-  if(input$runSim == 0) {return()
-                         }else{
+  if(input$runSim == 0) return()
+      isolate({
                            
-## ToDo: Should collect parameters here so the function will work with arbitrary numbers of gorups.                        
+#Gather the input parameters and set as correct vector                           
+if (input$group1==TRUE){
+ g1S <- input$group1Size 
+g1UD <- input$group1UiDist
+g1UM <- input$group1UiMean
+g1US <- input$group1UiStd
+g1ED <- input$group1EiDist
+g1EM <- input$group1EiMean
+g1ES <- input$group1EiStd
+g1A <- input$group1Alpha
+} else {
+  g1S <- NA
+  g1UD <- NA
+  g1UM <- NA
+  g1US <- NA
+  g1ED <- NA
+  g1EM <- NA
+  g1ES <- NA
+  g1A <- NA
+}         
+
+
+if (input$group2==TRUE){
+  g2S <- input$group2Size 
+  g2UD <- input$group2UiDist
+  g2UM <- input$group2UiMean
+  g2US <- input$group2UiStd
+  g2ED <- input$group2EiDist
+  g2EM <- input$group2EiMean
+  g2ES <- input$group2EiStd
+  g2A <- input$group2Alpha
+} else {
+  g2S <- NA
+  g2UD <- NA
+  g2UM <- NA
+  g2US <- NA
+  g2ED <- NA
+  g2EM <- NA
+  g2ES <- NA
+  g2A <- NA
+}  
+
+
+if (input$group3==TRUE){
+  g3S <- input$group3Size 
+  g3UD <- input$group3UiDist
+  g3UM <- input$group3UiMean
+  g3US <- input$group3UiStd
+  g3ED <- input$group3EiDist
+  g3EM <- input$group3EiMean
+  g3ES <- input$group3EiStd
+  g3A <- input$group3Alpha
+} else {
+  g3S <- NA
+  g3UD <- NA
+  g3UM <- NA
+  g3US <- NA
+  g3ED <- NA
+  g3EM <- NA
+  g3ES <- NA
+  g3A <- NA
+} 
 
 withProgress(                           
   aKMajorityRuleSimulation(folderName="Folder", 
                           numberOfIterations=input$numIter, 
-                          groupSize=c(input$group1Size,input$group2Size,input$group3Size),
-                          utilityDistribution=c("normal","normal","normal"), 
-                          utilityDistributionParameters=c(input$group1UiMean,input$group1UiStd,input$group2UiMean,input$group2UiStd,input$group3UiMean,input$group3UiStd), 
-                          errorDistribution=c("normal","normal","normal"), 
-                          errorDistributionParameters=c(input$group1EiMean,input$group1EiStd,input$group2EiMean,input$group1EiStd,input$group3EiMean,input$group1EiStd), 
-                          groupPostFailingProposalMeanUiIncrease=c(paste(input$group1Alpha),paste(input$group2Alpha),paste(input$group3Alpha)),
+                          groupSize=na.omit(c(g1S,g2S,g3S)),
+                          utilityDistribution=na.omit(c(g1UD,g2UD,g3UD)), 
+                          utilityDistributionParameters=na.omit(c(g1UM,g1US,g2UM,g2US,g3UM,g3US)), 
+                          errorDistribution=na.omit(c(g1ED,g2ED,g3ED)),  
+                          errorDistributionParameters=na.omit(c(g1EM,g1ES,g2EM,g2ES,g3EM,g3ES)), 
+                          groupPostFailingProposalMeanUiIncrease=na.omit(c(g1A,g2A,g3A)),
                           maximumNumberOfProposalsInASeries=input$maxProposals, 
                           perProposalDecisionCost=input$decisionCostPer, 
                           outputTo=getwd(),
@@ -36,23 +97,22 @@ withProgress(
                           plotPareto=FALSE,
                           printOutputToScreen=FALSE)
 , message='Calculation in Progress', detail='This may take a while.') # end withProgress
-                         } # end else
+
+      })
+
 }) #end reactive  
 
-
-  output$plotBest <- renderPlot({if(input$runSim==0) return(NULL)
+  output$plotBest <- renderPlot({if(input$runSim== 0) return(NULL)
   plotExpectedUtilityTotalCost(outputDataList = simOutput(), plotMeanEU = FALSE, plotBestEU = TRUE, plotWorstEU = FALSE)
                                 })
 
-  output$plotMean <- renderPlot({if(input$runSim==0) return(NULL)
+  output$plotMean <- renderPlot({if(input$runSim== 0) return(NULL)
   plotExpectedUtilityTotalCost(outputDataList = simOutput(), plotMeanEU = TRUE, plotBestEU = FALSE, plotWorstEU = FALSE)
                               })
   
-  output$plotWorst <- renderPlot({if(input$runSim==0) return(NULL)
+  output$plotWorst <- renderPlot({if(input$runSim== 0) return(NULL)
   plotExpectedUtilityTotalCost(outputDataList = simOutput(), plotMeanEU = FALSE, plotBestEU = FALSE, plotWorstEU = TRUE)
                                 })
-
-
 
 
 } # ends function definition
--- inst/kMajorityGUI/ui.R
+++ inst/kMajorityGUI/ui.R
@@ -8,38 +8,65 @@ shinyUI(fluidPage(
     
   fluidRow( 
     column(3,
+      checkboxInput("group1", "Use Group 1", value=TRUE),
+        conditionalPanel(
+          condition = "input.group1 == true",
       wellPanel(
         h2("Group 1"),
              numericInput("group1Size", "Size", 35),
-             numericInput("group1UiMean", "Utility Mean (Normal Dist.)", -.3),
-             numericInput("group1UiStd", "Utility Std. Dev. (Normal Dist.)", .2),
-             numericInput("group1EiMean", "Error Mean (Normal Dist.)", 0),
-             numericInput("group1EiStd", "Error Std. Dev. (Normal Dist.)", .3),
-             numericInput("group1Alpha", "Change in utility mean when proposal fails.", .1)
-      )),
+             selectInput("group1UiDist", label = "Utility Distribution", 
+                    choices = list("Normal" = "normal"), 
+                    selected = "normal"),
+             numericInput("group1UiMean", "Utility Mean", -.3),
+             numericInput("group1UiStd", "Utility Std. Dev.", .2),
+             selectInput("group1EiDist", label = "Utility Distribution", 
+                    choices = list("Normal" = "normal"), 
+                    selected = "normal"),
+             numericInput("group1EiMean", "Error Mean", 0),
+             numericInput("group1EiStd", "Error Std. Dev.", .3),
+             textInput("group1Alpha", "Change in utility mean when proposal fails.", .1)
+      ))),
              
     column(3,
-      wellPanel(
+      checkboxInput("group2", "Use Group 2", value=TRUE),
+           conditionalPanel(
+             condition = "input.group2 == true",
+        wellPanel(
         h2("Group 2"),
              numericInput("group2Size", "Size", 30),
-             numericInput("group2UiMean", "Utility Mean (Normal Dist.)", 0),
-             numericInput("group2UiStd", "Utility Std. Dev. (Normal Dist.)", .2),
-             numericInput("group2EiMean", "Error Mean (Normal Dist.)", 0),
-             numericInput("group2EiStd", "Error Std. Dev. (Normal Dist.)", .3),
-             numericInput("group2Alpha", "Change in utility mean when proposal fails.", .1)
+             selectInput("group2UiDist", label = "Utility Distribution", 
+                    choices = list("Normal" = "normal"), 
+                    selected = "normal"),
+             numericInput("group2UiMean", "Utility Mean", 0),
+             numericInput("group2UiStd", "Utility Std. Dev.", .2),
+             selectInput("group2EiDist", label = "Utility Distribution", 
+                    choices = list("Normal" = "normal"), 
+                    selected = "normal"),
+             numericInput("group2EiMean", "Error Mean", 0),
+             numericInput("group2EiStd", "Error Std. Dev.", .3),
+             textInput("group2Alpha", "Change in utility mean when proposal fails.", .1)
              
-      )),
+      ))),
              
     column(3,
+      checkboxInput("group3", "Use Group 3", value=TRUE),
+        conditionalPanel(
+         condition = "input.group3 == true",
       wellPanel(
         h2("Group 3"),
              numericInput("group3Size", "Size", 35),
-             numericInput("group3UiMean", "Utility Mean (Normal Dist.)", .3),
-             numericInput("group3UiStd", "Utility Std. Dev. (Normal Dist.)", .2),
-             numericInput("group3EiMean", "Error Mean (Normal Dist.)", 0),
-             numericInput("group3EiStd", "Error Std. Dev. (Normal Dist.)", .3),
-             numericInput("group3Alpha", "Change in utility mean when proposal fails.", .1)
-      )),     
+             selectInput("group3UiDist", label = "Utility Distribution", 
+                    choices = list("Normal" = "normal"), 
+                    selected = "normal"),
+             numericInput("group3UiMean", "Utility Mean", .3),
+             numericInput("group3UiStd", "Utility Std. Dev.", .2),
+            selectInput("group3EiDist", label = "Utility Distribution", 
+                    choices = list("Normal" = "normal"), 
+                    selected = "normal"),
+             numericInput("group3EiMean", "Error Mean", 0),
+             numericInput("group3EiStd", "Error Std. Dev.", .3),
+             textInput("group3Alpha", "Change in utility mean when proposal fails.", .1)
+      ))),     
         
     column(3,
       wellPanel(
